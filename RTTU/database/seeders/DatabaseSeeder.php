9e7b9268858e5c95450813ee2c97e929a0e66b44
HilmiAuf <muhammadhilmi130@gmail.com>
2022-11-06 22:52:01 +0700

    Update DatabaseSeeder.php
    Update Route

--- RTTU/database/seeders/DatabaseSeeder.php
+++ RTTU/database/seeders/DatabaseSeeder.php
@@ -31,6 +31,90 @@ class DatabaseSeeder extends Seeder
             'kendaraan' => "Bus"
         ]);
         Map::create([
+            'name' => "Diponegoro RRI",
+            'address' => "Gedung Kantor RRI Bandung, Cihaur Geulis, Cibeunying Kaler, Bandung City, West Java 40122",
+            'lat' => "-6.900707",
+            'lng' => "107.624155",
+            'kendaraan' => "Bus"
+        ])
+        Map::create([
+            'name' => "Gereja GII Hok Im Tong Dago",
+            'address' => "Jl. Cikapayang, Lb. Siliwangi, Kecamatan Coblong, Kota Bandung, Jawa Barat 40132",
+            'lat' => "-6.898668",
+            'lng' => "107.611939",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "TMB Surapati 04",
+            'address' => "Jl. Prof. Dr. Mochtar Kusumaatmadja, Lebakgede, Kecamatan Coblong, Kota Bandung, Jawa Barat 40132",
+            'lat' => "-6.899261",
+            'lng' => "107.618066",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Taman Lansia",
+            'address' => "Halte Perpustakaan Kementerian ESDM, Citarum, Kec. Bandung Wetan, Kota Bandung, Jawa Barat 40122",
+            'lat' => "-6.901060",
+            'lng' => "107.619840",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Natasha Supratman",
+            'address' => "Jl. Supratman No.78, Cihaur Geulis, Kec. Cibeunying Kaler, Kota Bandung, Jawa Barat 40122",
+            'lat' => "-6.903719",
+            'lng' => "107.628234",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Yudda Wastu Pramuka",
+            'address' => "Jl. Supratman 60, Cihapit, Kec. Bandung Wetan, Kota Bandung, Jawa Barat 40114",
+            'lat' => "-6.933951",
+            'lng' => "107.771266",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Taman Pramuka A",
+            'address' => "Jl. Taman Pramuka 178, Cihapit, Kec. Bandung Wetan, Kota Bandung, Jawa Barat 40114",
+            'lat' => "-6.910604",
+            'lng' => "107.627394",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte Supratman 1",
+            'address' => "SMP Santa Ursula, Jl. Anggrek No.1, Cihapit, Kec. Bandung Wetan, Kota Bandung, Jawa Barat 40114",
+            'lat' => "-6.910180",
+            'lng' => "107.632060",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Indomaret BKR",
+            'address' => "Jl. BKR Lkr. Sel. 8-4, Cijagra, Kec. Lengkong, Kota Bandung, Jawa Barat 40265",
+            'lat' => "-6.937263",
+            'lng' => "107.622104",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Simpang By Pass Soekarno Hatta A",
+            'address' => "JL Mohammad Toha, No. 19, Ciseureuh, Regol, Ciseureuh, Bandung, Kota Bandung, Jawa Barat 40255",
+            'lat' => "-6.947486",
+            'lng' => "107.609294",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Simpang Cileuny Atas",
+            'address' => "Cileunyi, Cileunyi Wetan, Cileunyi, Bandung Regency, West Java 40622",
+            'lat' => "-6.937445",
+            'lng' => "107.756117",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "IPDN A",
+            'address' => "JL Raya Jatinangor, No. 230, Jatinangor, Cibeusi, Bandung, Kabupaten Sumedang, Jawa Barat 45363",
+            'lat' => "-6.935299",
+            'lng' => "107.765215",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
             'name' => "Baltos",
             'address' => "Jatinangor bla bla",
             'lat' => "-6.898862",
