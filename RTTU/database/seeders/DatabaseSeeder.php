fe37de1ad34afd0ac889ed7784a92c45dbdb6fd3
HilmiAuf <muhammadhilmi130@gmail.com>
2022-11-27 16:24:02 +0700

    Update DatabaseSeeder.php
    Update Halte bus

--- RTTU/database/seeders/DatabaseSeeder.php
+++ RTTU/database/seeders/DatabaseSeeder.php
@@ -115,6 +115,146 @@ class DatabaseSeeder extends Seeder
             'kendaraan' => "Bus"
         ]);
         Map::create([
+            'name' => " Halte HDL 293 Citarum",
+            'address' => "Citarum, Bandung Wetan, Bandung City, West Java",
+            'lat' => "-6.901118",
+            'lng' => "107.624498",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte PT. Indonesia Comnets Plus",
+            'address' => "Sukamaju, Cibeunying Kidul, Bandung City, West Java",
+            'lat' => "-6.908033",
+            'lng' => "107.631070",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte Kiswah Umroh Wisata Syariah",
+            'address' => "Kacapiring, Batununggal, Bandung City, West Java",
+            'lat' => "-6.917383",
+            'lng' => "107.630823",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte Koperasi BKRKA",
+            'address' => "Kacapiring, Batununggal, Bandung City, West Java",
+            'lat' => "-6.918770",
+            'lng' => "107.631296",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte Hotel Sepuluh",
+            'address' => "South Lingkar, Lengkong, Bandung City, West Java",
+            'lat' => "-6.9306871",
+            'lng' => "107.62622",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte TDC Telkomsel Pelajar Pejuang",
+            'address' => "South Lingkar, Lengkong, Bandung City, West Java",
+            'lat' => "-6.9318981",
+            'lng' => "107.625753",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte Hotel Benua",
+            'address' => "Turangga, Lengkong, Bandung City, West Java",
+            'lat' => "-6.932523",
+            'lng' => "107.62565",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte PT. Citra Mandiri Selaras",
+            'address' => "Cigereleng, Regol, Bandung City, West Java",
+            'lat' => "-6.937660",
+            'lng' => "107.609933",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte Bkr Pt Inti",
+            'address' => "Cigereleng, Kec. Regol, Kota Bandung, Jawa Barat",
+            'lat' => "-6.937504",
+            'lng' => "107.6083",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte Sekolah Ganesha",
+            'address' => "Pelindung Hewan, Astanaanyar, Bandung City, West Java",
+            'lat' => "-6.938441",
+            'lng' => "107.60613",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte J&T Express Moh Toha",
+            'address' => "Karasak, Astanaanyar, Bandung City, West Java",
+            'lat' => "-6.9525966",
+            'lng' => "107.61095",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte Jl H Tatang SumantKarasak, Astanaanyar, Bandung City, West Javari",
+            'address' => "Karasak, Astanaanyar, Bandung City, West Java",
+            'lat' => "-6.954393",
+            'lng' => "107.61150",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte Terminal Cileunyi",
+            'address' => "Cileunyi Wetan, Cileunyi, Bandung Regency, West Java",
+            'lat' => "-6.940291",
+            'lng' => "107.75377",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte Tugu Batas Kota Sumedang Bandung" ,
+            'address' => "Cileunyi Wetan, Cileunyi, Bandung Regency, West Java",
+            'lat' => "-6.937592",
+            'lng' => "107.75718",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte Tugu Perbatasan Bandung Sumedang" ,
+            'address' => "Cibeusi, Jatinangor, Sumedang Regency, West Java",
+            'lat' => "-6.937863",
+            'lng' => "107.75716",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte IPDN B",
+            'address' => "Cibeusi, Jatinangor, Sumedang Regency, West Java",
+            'lat' => "-6.935727",
+            'lng' => "107.76507",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte Griya Jatinangor",
+            'address' => "Sayang, Jatinangor, Sumedang Regency, West Java",
+            'lat' => "-6.934667",
+            'lng' => "107.76835",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte Pangkalan Ojeg UNPAD",
+            'address' => "Sayang, Jatinangor, Sumedang Regency, West Java",
+            'lat' => "-6.932857",
+            'lng' => "107.77191",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte Pangkalan Bus Damri Jatinagor",
+            'address' => "Cikeruh, Jatinangor, Sumedang Regency, West Java",
+            'lat' => "-6.9319198",
+            'lng' => "107.7731",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
+            'name' => "Halte Bus Damri Jatinangor",
+            'address' => "Jl. Raya Cirebon - Bandung No.176, Cikeruh, Kec. Jatinangor, Kabupaten Sumedang, Jawa Barat 45363",
+            'lat' => "-6.931423",
+            'lng' => "107.775768",
+            'kendaraan' => "Bus"
+        ]);
+        Map::create([
             'name' => "Baltos",
             'address' => "Jatinangor bla bla",
             'lat' => "-6.898862",
